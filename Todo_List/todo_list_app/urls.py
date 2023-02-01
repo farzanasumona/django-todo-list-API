f308b368335fdb2bc821dd2e54dca0f37a16ddd4
Farzana Talukder Sumona <farzanasumona@gmail.com>
2023-02-02 00:32:23 +0100

    Added filter option

--- Todo_List/todo_list_app/urls.py
+++ Todo_List/todo_list_app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from todo_list_app import views
-from todo_list_app.views import todo_task, todo_task_create, todo
+from todo_list_app.views import todo_task_create, todo, TodoSearch
 
 urlpatterns = [
-    path('all/', todo_task),
     path('create/', todo_task_create),
     path('<int:pk>', todo),
+    path('all/', TodoSearch.as_view())
 
 ]
 
