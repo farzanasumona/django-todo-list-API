f1810523a2138276317160b9fa023358b9eae21d
Farzana Talukder Sumona <farzanasumona@gmail.com>
2023-02-07 18:23:46 +0100

    added patch test case

--- Todo_List/todo_list_app/tests/test_views.py
+++ Todo_List/todo_list_app/tests/test_views.py
@@ -1,6 +1,4 @@
-from rest_framework import status
-from rest_framework.test import APITestCase, APIRequestFactory
-
+from rest_framework.test import APITestCase
 from todo_list_app.models import Task
 from django.urls import reverse
 
@@ -8,29 +6,24 @@ from django.urls import reverse
 class BasicTest(APITestCase):
 
     def test_get_all_todos(self):
-        #booking_date = date.today() + timedelta(days=10)
-        #response = self.client.get(f'/parking/parkinglot/?=${booking_date}')
         response = self.client.get('/api/v1/tasks/all/')
         self.assertEqual(response.status_code, 200)
 
-class ApiViewsTests(APITestCase):
+
+class GetViewTest(APITestCase):
 
     url = reverse('get_by_id_task', args=[1])
 
     def test_get_by_id_todos(self):
+
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 404)
 
-        #given
         task = Task(title='title', is_done=True, desc='desc')
         task.save()
-        #when
-        response = self.client.get(self.url)
-        #then
+
+        response = self.client.get(reverse('get_by_id_task', kwargs={'pk': task.pk}), follow=True)
         self.assertEqual(response.status_code, 200)
-
-
-    #def test_update_by_id_todos(self):
 
 
 class DeleteViewTest(APITestCase):
@@ -38,24 +31,20 @@ class DeleteViewTest(APITestCase):
     url = reverse('get_by_id_task', args=[1])
 
     def test_delete_by_id_todos(self):
-        # given
+
         task = Task(title='title', is_done=True, desc='desc')
         task.save()
 
-
-        # when
-        #response = self.client.delete(self.url)
-
         response = self.client.delete(reverse('get_by_id_task', kwargs={'pk': task.pk}), follow=True)
-        # then
         self.assertEqual(response.status_code, 204)
+
 
 class PutViewTest(APITestCase):
 
     url = reverse('get_by_id_task', args=[1])
 
-    def test_delete_by_id_todos(self):
-        # given
+    def test_put_by_id_todos(self):
+
         task = Task(title='title', is_done=True, desc='desc')
         task.save()
 
@@ -72,6 +61,21 @@ class PutViewTest(APITestCase):
         task.refresh_from_db()
         self.assertEqual('ed', task.title)
 
+    def test_patch_by_id_todos(self):
+
+        task = Task(title='title', is_done=True, desc='desc')
+        task.save()
+
+        data = {
+
+            "title": "patch the title"
+        }
+
+        response = self.client.patch(reverse('get_by_id_task', kwargs={'pk': task.pk}), data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        task.refresh_from_db()
+        self.assertEqual('patch the title', task.title)
 
 
 class CreateViewTest(APITestCase):
